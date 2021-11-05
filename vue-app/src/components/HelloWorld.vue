2d30d74a7e4add0ff5ca9696de46d4d3209b09c1
Saskia Haug <ho4866@kit.edu>
2021-11-05 17:20:59 +0100

    Setup

---
+++ vue-app/src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+   <header>
+    <!-- Navbar -->
+    <MDBNavbar expand="lg" light bg="white" container position="sticky">
+      <MDBNavbarToggler target="#navbarExample01"></MDBNavbarToggler>
+      <MDBNavbarNav collapse="navbarExample01" class="mb-2 mb-lg-0">
+        <MDBNavbarItem to="#" active>
+          Home
+        </MDBNavbarItem>
+        <MDBNavbarItem to="#">
+          Features
+        </MDBNavbarItem>
+        <MDBNavbarItem to="#">
+          Pricing
+        </MDBNavbarItem>
+        <MDBNavbarItem to="#">
+          About
+        </MDBNavbarItem>
+      </MDBNavbarNav>
+    </MDBNavbar>
+    <!-- Navbar -->
+
+    <!-- Jumbotron -->
+    <div class="p-5 text-center bg-light" style="margin-top: 58px;">
+      <h1 class="mb-3">Heading</h1>
+      <h4 class="mb-3">Subheading</h4>
+      <MDBBtn tag="a" href="#" color="primary">Call to action</MDBBtn>
+    </div>
+    <!-- Jumbotron -->
+  </header>
+</template>
+
+<script>
+
+
+</script>
+
+<style scoped>
+
+</style>
